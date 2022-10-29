bfa523f3101d9bf5ee0ae03bd905d2d213953061
ubersk <kruchinin.s.s@gmail.com>
2022-10-29 12:26:22 +0300

    Добавьте файлы проекта.

---
+++ Vavilon/Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vavilon.Domain.Entities;
+using Vavilon.Infrastructure.Persistece;
+
+namespace Vavilon.Presentation.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<Product> Products { get; set; }
+        public MainWindowViewModel()
+        {
+            using(ApplicationDbContext context = new ApplicationDbContext())
+            {
+                Products = context.Products
+                    .ToList();
+            }
+        }
+    }
+}
